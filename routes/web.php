f3d50cd0c7cb59db916a21f81a5f868306733478
Vinicius Dias <carlosv775@gmail.com>
2019-04-10 19:20:35 -0300

    Subrecurso criado para buscar episódios de uma série

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ $router->group(['prefix' => 'series'], function () use ($router) {
     $router->get('{id}', 'SeriesController@show');
     $router->put('{id}', 'SeriesController@update');
     $router->delete('{id}', 'SeriesController@destroy');
+
+    $router->get('{serieId}/episodios', 'EpisodiosController@buscarPorSerie');
 });
 
 $router->group(['prefix' => 'episodios'], function () use ($router) {
