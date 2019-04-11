37430dafc82e5d064b80abe0151b69b3c7a81dda
Vinicius Dias <carlosv775@gmail.com>
2019-04-11 20:50:31 -0300

    Adicionando o campo _links nas models

--- app/Episodio.php
+++ app/Episodio.php
@@ -8,9 +8,24 @@ class Episodio extends Model
 {
     protected $fillable = ['numero', 'temporada', 'assistido', 'serie_id'];
     public $timestamps = false;
+    protected $appends = ['_links'];
+    protected $hidden = ['serie_id'];
 
     public function serie()
     {
         return $this->belongsTo(Serie::class);
+    }
+
+    public function getLinksAttribute()
+    {
+        return [
+            'self' => '/episodios/' . $this->id,
+            'serie' => '/series/' . $this->serie()->first()->id
+        ];
+    }
+
+    public function getAssistidoAttribute(bool $assistido): bool
+    {
+        return $assistido;
     }
 }
--- app/Serie.php
+++ app/Serie.php
@@ -8,9 +8,20 @@ class Serie extends Model
 {
     protected $fillable = ['nome'];
     public $timestamps = false;
+    protected $appends = ['_links'];
 
     public function episodios()
     {
         return $this->hasMany(Episodio::class);
+    }
+
+    public function getLinksAttribute()
+    {
+        return [
+            'self' => '/series/' . $this->id,
+            'episodios' => $this->episodios()->get()->map(function (Episodio $episodio) {
+                return '/episodios/' . $episodio->id;
+            }),
+        ];
     }
 }
